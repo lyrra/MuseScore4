204072e3285bdbe6e70aabc768d76e9f0cf84587
larry <larry@zorro>
2022-07-16 06:55:34 +0200

    CI test

--- build/ci/linux/build_utest.sh
+++ build/ci/linux/build_utest.sh
@@ -45,14 +45,6 @@ ${CC} --version
 echo " "
 cmake --version
 echo " "
-echo "VST3_SDK_PATH: $VST3_SDK_PATH"
-if [ -z "$VST3_SDK_PATH" ]; then 
-    echo "warning: not set VST3_SDK_PATH, build VST module disabled"
-    BUILD_VST=OFF
-else
-    BUILD_VST=ON
-fi
-
 echo "=== BUILD ==="
 
 MUSESCORE_REVISION=$(git rev-parse --short=7 HEAD)
@@ -64,6 +56,24 @@ MUSESCORE_BUILD_VST=$BUILD_VST \
 MUSESCORE_VST3_SDK_PATH=$VST3_SDK_PATH \
 MUSESCORE_DOWNLOAD_SOUNDFONT=OFF \
 MUSESCORE_BUILD_UNIT_TESTS=ON \
-bash ./ninja_build.sh -t debug          
+
+#-----------------------------------------------------------------------
+VERSION=$(cmake -P config.cmake | sed -n -e "s/^.*VERSION  *//p")
+CPUS=4
+export VERBOSE=1
+
+mkdir build.debug 2> /dev/null
+cd build.debug || exit 1
+
+cmake -G "Unix Makefiles" \
+      -DCMAKE_BUILD_TYPE=DEBUG \
+      -DVERBOSE=1 \
+      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+      -DBUILD_FOR_WINSTORE=OFF \
+      -DBUILD_TELEMETRY_MODULE=OFF \
+      -DBUILD_CRASH_REPORTER=OFF \
+      -DLOGLEVEL=9 \
+      ..
+make
 
 df -h .
