dc931af781d4f634883933fe109c14fe32c88a11
larry <larry@zorro>
2022-07-21 18:45:43 +0200

    scheme functions
    * match (very primitive)

---
+++ s7/lib.scm
@@ -0,0 +1,26 @@
+(define-macro (match expr case1)
+  (let ((lst (car case1))
+        (body (cdr case1)))
+  `(let ((ex ,expr))
+     (define (flatten lst)
+       (cond ((null? lst) ())
+             ((pair? lst)
+              (if (pair? (car lst))
+                  (append (flatten (car lst)) (flatten (cdr lst)))
+                  (cons (car lst) (flatten (cdr lst)))))
+             (#t lst)))
+
+     (define (structures-equal? l1 l2)
+       (if (pair? l1)
+           (and (pair? l2)
+                (structures-equal? (car l1) (car l2))
+                (structures-equal? (cdr l1) (cdr l2)))
+           (not (pair? l2))))
+
+     (if (not (structures-equal? ',lst ex))
+         (error 'test-error "~A and ~A do not match" ',lst ex))
+
+     (let ((names (flatten ',lst))
+           (vals (flatten ex)))
+       (apply (eval (list 'lambda names '(begin ,@body))) vals)))))
+
