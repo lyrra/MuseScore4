082766169f352b866b9dcd4da93e102802a9b18b
Larry Valkama <larryvalkama@protonmail.com>
2022-10-07 23:59:34 +0200

    ci linux build

--- build/ci/linux/build.sh
+++ build/ci/linux/build.sh
@@ -6,16 +6,13 @@ mkdir build.debug 2> /dev/null
 cd build.debug || exit 1
 
 cmake -G "Unix Makefiles" \
-      -DCMAKE_INSTALL_PREFIX=../win32install \
       -DCMAKE_BUILD_TYPE=DEBUG \
       -DVERBOSE=1 \
       -DBUILD_SHARED=ON \
       -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-      -DCMAKE_MAKE_PROGRAM=mingw32-make.exe \
       -DBUILD_FOR_WINSTORE=OFF \
       -DLOGLEVEL=1 \
       ..
-      #-DCMAKE_TOOLCHAIN_FILE=../tc-mingw.cmake  \
 
 make
 
