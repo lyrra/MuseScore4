7ff53957138fb7365a97db60a01e9e792c8d31d7
Larry Valkama <larryvalkama@protonmail.com>
2022-10-07 23:10:12 +0200

    ci linux build

--- build/ci/linux/build.sh
+++ build/ci/linux/build.sh
@@ -7,34 +7,26 @@ trap 'echo Build failed; exit 1' ERR
 
 df -h .
 
-TELEMETRY_TRACK_ID=""
 ARTIFACTS_DIR=build.artifacts
 BUILD_MODE=""
-BUILDTYPE=portable # portable build is the default build
 SUFFIX="" # appended to `mscore` command name to avoid conflicts (e.g. `mscore-dev`)
 OPTIONS=""
-BUILD_UI_MU4=ON    # not used, only for easier synchronization and compatibility
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -n|--number) BUILD_NUMBER="$2"; shift ;;
-        --telemetry) TELEMETRY_TRACK_ID="$2"; shift ;;
         --build_mode) BUILD_MODE="$2"; shift ;;
-        --build_mu4) BUILD_UI_MU4="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-if [ -z "$BUILD_NUMBER" ]; then echo "error: not set BUILD_NUMBER"; exit 1; fi
-if [ -z "$TELEMETRY_TRACK_ID" ]; then TELEMETRY_TRACK_ID=""; fi
 if [ -z "$BUILD_MODE" ]; then BUILD_MODE=$(cat $ARTIFACTS_DIR/env/build_mode.env); fi
 
 MUSESCORE_BUILD_CONFIG=dev
 
 case "${BUILD_MODE}" in
 "devel_build")   MUSESCORE_BUILD_CONFIG=dev; SUFFIX=-dev;;
-"nightly_build") MUSESCORE_BUILD_CONFIG=dev; SUFFIX=-nightly;;
 "testing_build") MUSESCORE_BUILD_CONFIG=testing; SUFFIX=-testing;;
 "stable_build")  MUSESCORE_BUILD_CONFIG=release; SUFFIX="";;
 "mtests")        MUSESCORE_BUILD_CONFIG=dev; BUILDTYPE=installdebug; OPTIONS="USE_SYSTEM_FREETYPE=ON UPDATE_CACHE=FALSE PREFIX=$ARTIFACTS_DIR/software";;
@@ -46,7 +38,6 @@ fi
 
 echo "MUSESCORE_BUILD_CONFIG: $MUSESCORE_BUILD_CONFIG"
 echo "BUILD_NUMBER: $BUILD_NUMBER"
-echo "TELEMETRY_TRACK_ID: $TELEMETRY_TRACK_ID"
 echo "BUILD_MODE: $BUILD_MODE"
 echo "BUILDTYPE: $BUILDTYPE"
 echo "OPTIONS: $OPTIONS"
