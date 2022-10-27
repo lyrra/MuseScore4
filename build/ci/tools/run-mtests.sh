e1a76db37e382922d47ebac8f670fbf16ec95fa8
larry <larry@zorro>
2022-10-27 12:11:19 +0200

    ci run mtests

--- build/ci/tools/run-mtests.sh
+++ build/ci/tools/run-mtests.sh
@@ -7,6 +7,8 @@ trap 'echo Run tests failed; exit 1' ERR
 
 source ./../musescore_environment.sh
 
+D=`pwd`/build.debug
+
 cd build.debug/mtest
 
 # run the mtests in "minimal" platform for headless systems
@@ -14,11 +16,12 @@ cd build.debug/mtest
 export QT_QPA_PLATFORM=minimal:enable_fonts
 # if AddressSanitizer was used, disable leak detection
 export ASAN_OPTIONS=detect_leaks=0:new_delete_type_mismatch=0
-export LD_LIBRARY_PATH=../muxtools:../libmscore:../muxlib:../mscore:../importexport:.
+export LD_LIBRARY_PATH=$D/muxtools:$D/libmscore:$D/muxlib:$D/mscore:$D/importexport:$D/mtest
 
 echo "---- run ctest ----"
 ctest --output-on-failure
 echo "---- run mtest ----"
+ldd mtest
 ./mtest $*
 echo "---- run guile tests ----"
 sh run-guile.sh $*
