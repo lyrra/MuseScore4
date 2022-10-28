476137c9eaa1f389b1003058f58c7bf7503af055
larry <larry@zorro>
2022-10-29 00:37:24 +0200

    disable adress-sanitizer during mtest

--- build/ci/tools/build-mtests.sh
+++ build/ci/tools/build-mtests.sh
@@ -30,7 +30,7 @@ pushd mtest
 # enable fonts handling
 export QT_QPA_PLATFORM=minimal:enable_fonts
 # if AddressSanitizer was used, disable leak detection
-export ASAN_OPTIONS=detect_leaks=0:new_delete_type_mismatch=0
+#export ASAN_OPTIONS=detect_leaks=0:new_delete_type_mismatch=0
 
 export GUILE_SYSTEM_COMPILED_PATH=/mingw64/lib/guile/3.0/ccache
 
