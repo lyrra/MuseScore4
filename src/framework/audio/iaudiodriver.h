musescore/MuseScore

GUI filter musescore-ingress midiport

src/framework/audio/iaudiodriver.h
@@ -84,7 +84,7 @@ public:
     virtual std::vector<unsigned int> availableOutputDeviceBufferSizes() const = 0;
 
     virtual bool pushMidiEvent(mu::midi::Event&) = 0;
-    virtual std::vector<mu::midi::MidiDevice> availableMidiDevices() const = 0;
+    virtual std::vector<mu::midi::MidiDevice> availableMidiDevices(mu::midi::MidiPortDirection dir) const = 0;
 
     virtual void resume() = 0;
     virtual void suspend() = 0;
@@ -99,7 +99,7 @@ public:
     virtual bool isOpened() const = 0;
     virtual bool pushMidiEvent(mu::midi::Event&) = 0;
     virtual void registerMidiInputQueue(async::Channel<mu::midi::tick_t, mu::midi::Event >*) = 0;
-    virtual std::vector<mu::midi::MidiDevice> availableMidiDevices() const = 0;
+    virtual std::vector<mu::midi::MidiDevice> availableMidiDevices(mu::midi::MidiPortDirection dir) const = 0;
 
     IAudioDriver::Spec m_spec; // current running spec
     std::string m_deviceId;

src/framework/audio/internal/platform/jack/jackaudiodriver.cpp
@@ -353,25 +353,36 @@ void JackDriverState::registerMidiInputQueue(async::Channel<mu::midi::tick_t, mu
     m_eventReceived = midiInputQueue;
 }
 
-std::vector<mu::midi::MidiDevice> JackDriverState::availableMidiDevices() const
+std::vector<mu::midi::MidiDevice> JackDriverState::availableMidiDevices(mu::midi::MidiPortDirection direction) const
 {
     std::vector<mu::midi::MidiDevice> ports;
     std::vector<mu::midi::MidiDevice> ret;
     jack_client_t* client = static_cast<jack_client_t*>(m_jackDeviceHandle);
-    const char** prts = jack_get_ports(client, 0, 0, 0);
+    const char** prts = jack_get_ports(client, 0, "midi", 0);
     int devIndex = 0;
     for (const char** p = prts; p && *p; ++p) {
         jack_port_t* port = jack_port_by_name(client, *p);
         int flags = jack_port_flags(port);
-        if (!(flags & JackPortIsInput)) {
+
+        if ((flags & JackPortIsInput)
+            && direction == mu::midi::MidiPortDirection::Output) {
             continue;
         }
+        if ((flags & JackPortIsOutput)
+            && direction == mu::midi::MidiPortDirection::Input) {
+            continue;
+        }
+
         char buffer[128];
         strncpy(buffer, *p, sizeof(buffer) - 1);
         buffer[sizeof(buffer) - 1] = 0;
+
         if (strncmp(buffer, "MuseScore", 9) == 0) {
             continue;
         }
+
+        LOGE("adding jack-port %s", buffer);
+
         mu::midi::MidiDevice dev;
         dev.name = buffer;
         dev.id = makeUniqueDeviceId(devIndex++, 0, 0);

src/framework/audio/internal/platform/jack/jackaudiodriver.h
@@ -44,7 +44,7 @@ public:
 
     std::string deviceName() const;
     void deviceName(const std::string newDeviceName);
-    std::vector<mu::midi::MidiDevice> availableMidiDevices() const;
+    std::vector<mu::midi::MidiDevice> availableMidiDevices(mu::midi::MidiPortDirection direction) const;
 
     void* m_jackDeviceHandle = nullptr;
     float* m_buffer = nullptr;

src/framework/audio/internal/platform/lin/linuxaudiodriver.cpp
@@ -238,10 +238,10 @@ bool LinuxAudioDriver::pushMidiEvent(mu::midi::Event& e)
     return false;
 }
 
-std::vector<mu::midi::MidiDevice> LinuxAudioDriver::availableMidiDevices() const
+std::vector<mu::midi::MidiDevice> LinuxAudioDriver::availableMidiDevices(mu::midi::MidiPortDirection direction) const
 {
     if (m_current_audioDriverState) {
-        return m_current_audioDriverState->availableMidiDevices();
+        return m_current_audioDriverState->availableMidiDevices(direction);
     }
     std::vector<mu::midi::MidiDevice> x;
     return x;

src/framework/audio/internal/platform/lin/linuxaudiodriver.h
@@ -58,7 +58,7 @@ public:
 
     std::vector<unsigned int> availableOutputDeviceBufferSizes() const override;
     bool pushMidiEvent(mu::midi::Event& e) override;
-    std::vector<mu::midi::MidiDevice> availableMidiDevices() const override;
+    std::vector<mu::midi::MidiDevice> availableMidiDevices(mu::midi::MidiPortDirection direction) const override;
 
     void resume() override;
     void suspend() override;

src/framework/audio/internal/platform/osx/osxaudiodriver.h
@@ -70,7 +70,7 @@ public:
 
     std::vector<unsigned int> availableOutputDeviceBufferSizes() const override;
     bool pushMidiEvent(mu::midi::Event& e) override;
-    std::vector<mu::midi::MidiDevice> availableMidiDevices() const override;
+    std::vector<mu::midi::MidiDevice> availableMidiDevices(mu::midi::MidiPortDirection dir) const override;
 
 private:
     static void OnFillBuffer(void* context, OpaqueAudioQueue* queue, AudioQueueBuffer* buffer);

src/framework/audio/internal/platform/win/wasapiaudiodriver.cpp
@@ -313,7 +313,7 @@ bool WasapiAudioDriver::pushMidiEvent(mu::midi::Event&)
     return true;
 }
 
-std::vector<mu::midi::MidiDevice> WasapiAudioDriver::availableMidiDevices() const
+std::vector<mu::midi::MidiDevice> WasapiAudioDriver::availableMidiDevices(mu::midi::MidiPortDirection dir) const
 {
     std::vector<mu::midi::MidiDevice> x;
     return x; // dummy

lyrra/MuseScore4

src/framework/audio/iaudiodriver.h
@@ -91,7 +91,7 @@ public:
 
     virtual std::vector<unsigned int> availableOutputDeviceSampleRates() const = 0;
     virtual bool pushMidiEvent(muse::midi::Event&) = 0;
-    virtual std::vector<muse::midi::MidiDevice> availableMidiDevices() const = 0;
+    virtual std::vector<muse::midi::MidiDevice> availableMidiDevices(muse::midi::MidiPortDirection dir) const = 0;
 
     virtual void resume() = 0;
     virtual void suspend() = 0;
@@ -106,7 +106,7 @@ public:
     virtual bool isOpened() const = 0;
     virtual bool pushMidiEvent(muse::midi::Event&) = 0;
     virtual void registerMidiInputQueue(async::Channel<muse::midi::tick_t, muse::midi::Event >) = 0;
-    virtual std::vector<muse::midi::MidiDevice> availableMidiDevices() const = 0;
+    virtual std::vector<muse::midi::MidiDevice> availableMidiDevices(muse::midi::MidiPortDirection dir) const = 0;
 
     IAudioDriver::Spec m_spec; // current running spec
     std::string m_deviceId;

src/framework/audio/internal/platform/jack/jackaudiodriver.cpp
@@ -353,12 +353,12 @@ void JackDriverState::registerMidiInputQueue(async::Channel<muse::midi::tick_t, 
     m_eventReceived = midiInputQueue;
 }
 
-std::vector<muse::midi::MidiDevice> JackDriverState::availableMidiDevices() const
+std::vector<muse::midi::MidiDevice> JackDriverState::availableMidiDevices(muse::midi::MidiPortDirection direction) const
 {
     std::vector<muse::midi::MidiDevice> ports;
     std::vector<muse::midi::MidiDevice> ret;
     jack_client_t* client = static_cast<jack_client_t*>(m_jackDeviceHandle);
-    const char** prts = jack_get_ports(client, 0, 0, 0);
+    const char** prts = jack_get_ports(client, 0, "midi", 0);
     if (!prts) {
         return ports;
     }
@@ -366,12 +366,20 @@ std::vector<muse::midi::MidiDevice> JackDriverState::availableMidiDevices() cons
     for (const char** p = prts; p && *p; ++p) {
         jack_port_t* port = jack_port_by_name(client, *p);
         int flags = jack_port_flags(port);
-        if (!(flags & JackPortIsInput)) {
+
+        if ((flags & JackPortIsInput)
+            && direction == muse::midi::MidiPortDirection::Output) {
             continue;
         }
+        if ((flags & JackPortIsOutput)
+            && direction == muse::midi::MidiPortDirection::Input) {
+            continue;
+        }
+
         char buffer[128];
         strncpy(buffer, *p, sizeof(buffer) - 1);
         buffer[sizeof(buffer) - 1] = 0;
+
         if (strncmp(buffer, "MuseScore", 9) == 0) {
             continue;
         }

src/framework/audio/internal/platform/jack/jackaudiodriver.h
@@ -44,7 +44,7 @@ public:
 
     std::string deviceName() const;
     void deviceName(const std::string newDeviceName);
-    std::vector<muse::midi::MidiDevice> availableMidiDevices() const;
+    std::vector<muse::midi::MidiDevice> availableMidiDevices(muse::midi::MidiPortDirection direction) const;
 
     void* m_jackDeviceHandle = nullptr;
     float* m_buffer = nullptr;

src/framework/audio/internal/platform/lin/linuxaudiodriver.cpp
@@ -282,10 +282,10 @@ bool LinuxAudioDriver::pushMidiEvent(muse::midi::Event& e)
     return false;
 }
 
-std::vector<muse::midi::MidiDevice> LinuxAudioDriver::availableMidiDevices() const
+std::vector<muse::midi::MidiDevice> LinuxAudioDriver::availableMidiDevices(muse::midi::MidiPortDirection direction) const
 {
     if (m_current_audioDriverState) {
-        return m_current_audioDriverState->availableMidiDevices();
+        return m_current_audioDriverState->availableMidiDevices(direction);
     }
     std::vector<muse::midi::MidiDevice> x;
     return x;

src/framework/audio/internal/platform/lin/linuxaudiodriver.h
@@ -62,7 +62,7 @@ public:
 
     std::vector<unsigned int> availableOutputDeviceBufferSizes() const override;
     bool pushMidiEvent(muse::midi::Event& e) override;
-    std::vector<muse::midi::MidiDevice> availableMidiDevices() const override;
+    std::vector<muse::midi::MidiDevice> availableMidiDevices(muse::midi::MidiPortDirection direction) const override;
 
     unsigned int outputDeviceSampleRate() const override;
     bool setOutputDeviceSampleRate(unsigned int sampleRate) override;

src/framework/audio/internal/platform/osx/osxaudiodriver.h
@@ -68,7 +68,7 @@ public:
 
     std::vector<unsigned int> availableOutputDeviceBufferSizes() const override;
     bool pushMidiEvent(muse::midi::Event& e) override;
-    std::vector<muse::midi::MidiDevice> availableMidiDevices() const override;
+    std::vector<muse::midi::MidiDevice> availableMidiDevices(muse::midi::MidiPortDirection dir) const override;
 
     unsigned int outputDeviceSampleRate() const override;
     bool setOutputDeviceSampleRate(unsigned int sampleRate) override;

src/framework/audio/internal/platform/win/wasapiaudiodriver.cpp
@@ -375,7 +375,7 @@ bool WasapiAudioDriver::pushMidiEvent(muse::midi::Event&)
     return true;
 }
 
-std::vector<muse::midi::MidiDevice> WasapiAudioDriver::availableMidiDevices() const
+std::vector<muse::midi::MidiDevice> WasapiAudioDriver::availableMidiDevices(muse::midi::MidiPortDirection dir) const
 {
     std::vector<muse::midi::MidiDevice> x;
     return x; // dummy